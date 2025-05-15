1d0c351649dfdf77489d468941336bb5f05fab59
eul-adrian <tradrian24@gmail.com>
2025-05-15 10:11:14 +0200

    Reinitializare Git și reconectare la remote

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>VetAI Dashboard</h1>
+      <p>Acesta este locul pentru Chatbotul AI 🧠</p>
+    </div>
+  )
+}
